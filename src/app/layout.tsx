1d1d8442733aea31161f83912eb98f227b6281ff
Anya Joy <anyajoy@Anyas-MacBook-Air.local>
2023-06-15 23:48:55 +0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
+import { Metadata } from 'next';
 
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Anya Joy | Full Stack Developer'
+};
 
 export default function RootLayout({
   children,
